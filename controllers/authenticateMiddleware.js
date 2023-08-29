06f0dc03a0fe3ad445bad53e8843b826d33c998f
m.backi <backimichal@gmail.com>
2023-08-29 12:39:56 +0200

    Update authenticateMiddleware.js

--- controllers/authenticateMiddleware.js
+++ controllers/authenticateMiddleware.js
@@ -6,20 +6,14 @@ const authenticateMiddleware = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
     if (!token) {
-      console.log("No token provided");
       return res.status(401).json({ message: "Not authorized" });
     }
 
-    console.log("Token:", token);
-
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded:", decoded);
 
     const user = await User.findById(decoded.userId);
-    console.log("User:", user);
 
     if (!user) {
-      console.log("User not found");
       return res.status(401).json({ message: "Not authorized" });
     }
 
@@ -27,7 +21,6 @@ const authenticateMiddleware = async (req, res, next) => {
     req.token = token;
     next();
   } catch (error) {
-    console.error("Authentication error:", error);
     res.status(401).json({ message: "Not authorized" });
   }
 };
