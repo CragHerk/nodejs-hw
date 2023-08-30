d7114459cb32ef1c0cfb0eefcdeb86cdbdc8552c
m.backi <backimichal@gmail.com>
2023-08-30 14:59:05 +0200

    Update authenticateMiddleware.js

--- controllers/authenticateMiddleware.js
+++ controllers/authenticateMiddleware.js
@@ -22,6 +22,20 @@ passport.use(
   })
 );
 
-const authenticateMiddleware = passport.authenticate("jwt", { session: false });
+const authenticateMiddleware = (req, res, next) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    const token = req.headers["authorization"].split(" ")[1];
+    if (!user || err || !token || token !== user.token) {
+      return res.status(401).json({
+        status: "error",
+        code: 401,
+        message: "Unauthorized",
+        data: "Unauthorized",
+      });
+    }
+    req.user = user;
+    next();
+  })(req, res, next);
+};
 
 module.exports = authenticateMiddleware;
